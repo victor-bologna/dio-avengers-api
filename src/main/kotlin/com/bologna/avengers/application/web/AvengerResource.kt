60e11e1fc7da51e5524e5350652f6c6acc2a39f9
Victor Bologna <victorbologna@hotmail.com>
2026-03-25 11:41:37 -0300

    Removed unnecessary import.

--- src/main/kotlin/com/bologna/avengers/application/web/AvengerResource.kt
+++ src/main/kotlin/com/bologna/avengers/application/web/AvengerResource.kt
@@ -5,15 +5,7 @@ import com.bologna.avengers.application.web.resource.response.AvengerResponse
 import com.bologna.avengers.domain.avenger.AvengerRepository
 import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PatchMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.net.URI
 
 private const val API_PATH = "/api/v1/avengers"
