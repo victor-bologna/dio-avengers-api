8bb7b447a8087be96bcb3d98a0613bf48a82305b
Victor Bologna <victorbologna@hotmail.com>
2026-03-25 10:31:53 -0300

    Initial commit

---
+++ src/test/kotlin/com/bologna/avengers/DioAvengersApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bologna.avengers
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DioAvengersApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
